d830472b7203fc3199047793c74543646a5751f3
miran.tuhtan <miran.tuhtan@uniri.hr>
2017-03-21 08:40:23 +0100

    tabs to spaces

--- ticky.py
+++ ticky.py
@@ -38,9 +38,9 @@ PLAYER_X = 21
 PLAYER_O_WIN = PLAYER_O * 3
 PLAYER_X_WIN = PLAYER_X * 3
 
-CONT_GAME 		= 10
-DRAW_GAME 		= 20
-QUIT_GAME 		= 30
+CONT_GAME         = 10
+DRAW_GAME         = 20
+QUIT_GAME         = 30
 
 XMARGIN = int((WINDOWWIDTH - (TILESIZE * BOARDWIDTH + (BOARDWIDTH - 1))) / 2)
 YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
@@ -48,110 +48,110 @@ YMARGIN = int((WINDOWHEIGHT - (TILESIZE * BOARDHEIGHT + (BOARDHEIGHT - 1))) / 2)
 choice = 0
 
 def check_draw_game(board):
-	return True if sum(board)%10 == 9 else False
+    return True if sum(board)%10 == 9 else False
 
 def check_win_game(board):
-	if ((board[0]+board[1]+board[2]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[0]+board[1]+board[2]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[3]+board[4]+board[5]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[3]+board[4]+board[5]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[6]+board[7]+board[8]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[6]+board[7]+board[8]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[0]+board[3]+board[6]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[0]+board[3]+board[6]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[1]+board[4]+board[7]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[1]+board[4]+board[7]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[2]+board[5]+board[8]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[2]+board[5]+board[8]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[0]+board[4]+board[8]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[0]+board[4]+board[8]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif ((board[2]+board[4]+board[6]) == PLAYER_O_WIN):
-		return PLAYER_O
-	elif ((board[2]+board[4]+board[6]) == PLAYER_X_WIN):
-		return PLAYER_X
-	elif check_draw_game(board):
-		return DRAW_GAME
-	else:
-		return CONT_GAME
+    if ((board[0]+board[1]+board[2]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[0]+board[1]+board[2]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[3]+board[4]+board[5]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[3]+board[4]+board[5]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[6]+board[7]+board[8]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[6]+board[7]+board[8]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[0]+board[3]+board[6]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[0]+board[3]+board[6]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[1]+board[4]+board[7]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[1]+board[4]+board[7]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[2]+board[5]+board[8]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[2]+board[5]+board[8]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[0]+board[4]+board[8]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[0]+board[4]+board[8]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif ((board[2]+board[4]+board[6]) == PLAYER_O_WIN):
+        return PLAYER_O
+    elif ((board[2]+board[4]+board[6]) == PLAYER_X_WIN):
+        return PLAYER_X
+    elif check_draw_game(board):
+        return DRAW_GAME
+    else:
+        return CONT_GAME
 
 def unit_score(winner,depth):
-	if winner == DRAW_GAME:
-		return 0
-	else:
-		return 10 - depth if winner == PLAYER_X else depth - 10
+    if winner == DRAW_GAME:
+        return 0
+    else:
+        return 10 - depth if winner == PLAYER_X else depth - 10
 
 def get_available_step(board):
-	ava_step = []
-	for i in range(9):
-		if board[i] == BLANK:
-			ava_step.append(i)
-	return ava_step
+    ava_step = []
+    for i in range(9):
+        if board[i] == BLANK:
+            ava_step.append(i)
+    return ava_step
 
 
 
 def minmax(board, depth):
-	global choice
-	result = check_win_game(board)
-	if result != CONT_GAME:
-		return unit_score(result, depth)
+    global choice
+    result = check_win_game(board)
+    if result != CONT_GAME:
+        return unit_score(result, depth)
 
-	depth += 1
-	scores = []
-	steps = []
+    depth += 1
+    scores = []
+    steps = []
 
-	for step in get_available_step(board):
-		score = minmax(update_state(board,step,depth),depth)
-		scores.append(score)
-		steps.append(step)
+    for step in get_available_step(board):
+        score = minmax(update_state(board,step,depth),depth)
+        scores.append(score)
+        steps.append(step)
 
-	if depth % 2 == 1:
-		max_value_index = scores.index(max(scores))
-		choice = steps[max_value_index]
-		return max(scores)
-	else:
-		min_value_index = scores.index(min(scores))
-		choice = steps[min_value_index]
-		return min(scores)
+    if depth % 2 == 1:
+        max_value_index = scores.index(max(scores))
+        choice = steps[max_value_index]
+        return max(scores)
+    else:
+        min_value_index = scores.index(min(scores))
+        choice = steps[min_value_index]
+        return min(scores)
 
 def update_state(board, step, depth):
-	board = list(board)
-	if depth % 2 ==1:
-		board[step] = PLAYER_X
-	else:
-		board[step] = PLAYER_O
-	return board
+    board = list(board)
+    if depth % 2 ==1:
+        board[step] = PLAYER_X
+    else:
+        board[step] = PLAYER_O
+    return board
 
 def update_board(board, step, player):
-	board[step] = player
+    board[step] = player
 
 def change_to_player(player):
-	if player == PLAYER_O:
-		return 'O'
-	elif player == PLAYER_X:
-		return 'X'
-	elif player == BLANK:
-		return '-'
+    if player == PLAYER_O:
+        return 'O'
+    elif player == PLAYER_X:
+        return 'X'
+    elif player == BLANK:
+        return '-'
 
 
 #def draw_the_game(board):
-#	for i in range(3):
-#		for j in range(3):
-#			print change_to_player(board[i*3+j]),
-#		print ' '
+#    for i in range(3):
+#        for j in range(3):
+#            print change_to_player(board[i*3+j]),
+#        print ' '
 
 def drawBoard(board, message):
     DISPLAYSURF.fill(BGCOLOR)
@@ -161,7 +161,7 @@ def drawBoard(board, message):
 
     for tilex in range(3):
         for tiley in range(3):
-        	if board[tilex*3+tiley] != BLANK:
+            if board[tilex*3+tiley] != BLANK:
                  drawTile(tilex, tiley, board[tilex*3+tiley])
 
     left, top = getLeftTopOfTile(0, 0)
@@ -195,10 +195,10 @@ def drawTile(tilex, tiley, symbol, adjx=0, adjy=0):
 
 
 def symbol_to_str(symbol):
-	if symbol == PLAYER_O:
-		return 'O'
-	elif symbol ==PLAYER_X:
-		return 'X'
+    if symbol == PLAYER_O:
+        return 'O'
+    elif symbol ==PLAYER_X:
+        return 'X'
 
 def getSpotClicked(x, y):
     # from the x & y pixel coordinates, get the x & y board coordinates
@@ -211,68 +211,68 @@ def getSpotClicked(x, y):
     return (None, None)
 
 def board_to_step(spotx, spoty):
-	return spotx * 3 + spoty
+    return spotx * 3 + spoty
 
 def main():
-	global FPSCLOCK, DISPLAYSURF, BASICFONT, NEW_SURF, NEW_RECT
-	pygame.init()
-	FPSCLOCK = pygame.time.Clock()
-	DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
-	pygame.display.set_caption('Ticky')
-	BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
-	NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
-	board = [BLANK] * 9
-	flag = 0
-	msg = "Ticky - Unbeatable Tic Tac Toe AI"
-	drawBoard(board,msg)
-	pygame.display.update()
-	computer_turn = 0
-	while True:
-		(spotx, spoty) = (None, None)
-		for event in pygame.event.get():
-			if event.type == MOUSEBUTTONUP:
-				spotx, spoty = getSpotClicked(event.pos[0], event.pos[1])
-				if (spotx, spoty) == (None, None):
-					if NEW_RECT.collidepoint(event.pos):
-						board = [BLANK] * 9
-						flag = 0
-						msg = "Ticky - Unbeatable Tic Tac Toe AI"
-						drawBoard(board, msg)
-						pygame.display.update()
-		if (spotx, spoty) != (None, None):
-			if not flag:
-				player = PLAYER_O
-				next_step = board_to_step(spotx, spoty)
-				update_board(board,next_step, player)
-				result = check_win_game(board)
-				if result != CONT_GAME:
-					flag = 1
-					if result == PLAYER_X:
-						msg = "Ticky win!"
-					elif result == PLAYER_O:
-						msg = "you win, awesome!"
-					else:
-						msg = "Draw game"
-				drawBoard(board, msg)
-				pygame.display.update()
-				computer_turn = 1
-			if not flag and computer_turn:
-				score = minmax(board, 0)
-				player = PLAYER_X
-				update_board(board, choice, player)
-				result = check_win_game(board)
-				if result != CONT_GAME:
-					flag = 1
-					if result == PLAYER_X:
-						msg = "Ticky win!"
-					elif result == PLAYER_O:
-						msg = "you win, awesome!"
-					else:
-						msg = "Draw game"
-				drawBoard(board, msg)
-				pygame.display.update()
-				computer_turn = 0
+    global FPSCLOCK, DISPLAYSURF, BASICFONT, NEW_SURF, NEW_RECT
+    pygame.init()
+    FPSCLOCK = pygame.time.Clock()
+    DISPLAYSURF = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+    pygame.display.set_caption('Ticky')
+    BASICFONT = pygame.font.Font('freesansbold.ttf', BASICFONTSIZE)
+    NEW_SURF,   NEW_RECT   = makeText('New Game', TEXTCOLOR, TILECOLOR, WINDOWWIDTH - 120, WINDOWHEIGHT - 60)
+    board = [BLANK] * 9
+    flag = 0
+    msg = "Ticky - Unbeatable Tic Tac Toe AI"
+    drawBoard(board,msg)
+    pygame.display.update()
+    computer_turn = 0
+    while True:
+        (spotx, spoty) = (None, None)
+        for event in pygame.event.get():
+            if event.type == MOUSEBUTTONUP:
+                spotx, spoty = getSpotClicked(event.pos[0], event.pos[1])
+                if (spotx, spoty) == (None, None):
+                    if NEW_RECT.collidepoint(event.pos):
+                        board = [BLANK] * 9
+                        flag = 0
+                        msg = "Ticky - Unbeatable Tic Tac Toe AI"
+                        drawBoard(board, msg)
+                        pygame.display.update()
+        if (spotx, spoty) != (None, None):
+            if not flag:
+                player = PLAYER_O
+                next_step = board_to_step(spotx, spoty)
+                update_board(board,next_step, player)
+                result = check_win_game(board)
+                if result != CONT_GAME:
+                    flag = 1
+                    if result == PLAYER_X:
+                        msg = "Ticky win!"
+                    elif result == PLAYER_O:
+                        msg = "you win, awesome!"
+                    else:
+                        msg = "Draw game"
+                drawBoard(board, msg)
+                pygame.display.update()
+                computer_turn = 1
+            if not flag and computer_turn:
+                score = minmax(board, 0)
+                player = PLAYER_X
+                update_board(board, choice, player)
+                result = check_win_game(board)
+                if result != CONT_GAME:
+                    flag = 1
+                    if result == PLAYER_X:
+                        msg = "Ticky win!"
+                    elif result == PLAYER_O:
+                        msg = "you win, awesome!"
+                    else:
+                        msg = "Draw game"
+                drawBoard(board, msg)
+                pygame.display.update()
+                computer_turn = 0
 
 
 if __name__ == '__main__':
-	main()
+    main()
